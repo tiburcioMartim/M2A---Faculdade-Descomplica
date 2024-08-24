9ee9df8d8af036c075bd13080491f1184b042e35
Martim Tiburcio <tiburciomartim@gmail.com>
2024-08-24 06:59:51 -0300

    class 6 - working with for's

---
+++ desenvolvimento dinamico/class 7 - working with For/for.js
@@ -0,0 +1,42 @@
+let cars = [
+    { model: "Audio A3", brand: "Audi", year: "2020" },
+    { model: "Compass", brand: "Jeep", year: "2021" }
+]
+
+// let a = 1;
+// while (a <= 10) {
+//     console.log(a);
+//     a++
+// }
+
+
+let b = 1;
+do {
+    console.log(b);
+    b++
+
+} while (b < 6)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for (let car of cars) {
+//     console.log(car.year);
+// }
+
+// for (let caracteristic in cars) {
+//     console.log(cars[caracteristic].brand);
+// }
